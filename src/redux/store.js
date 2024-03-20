b6f1f2dc0075e6d4f9cc16424ef69d36b82843d9
lilshudd <max.fedorak10@gmail.com>
2024-03-20 11:30:14 +0200

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,46 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (error) {
+    console.error("Error saving state to localStorage:", error);
+  }
+};
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Error loading state from localStorage:", error);
+    return undefined;
+  }
+};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filters: filtersReducer,
 });
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["contacts"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedState = loadState();
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
+  preloadedState: persistedState,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-export const persistor = persistStore(store);
+store.subscribe(() => {
+  saveState(store.getState());
+});
